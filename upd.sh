056a1c5772c0324d9d46d11f5dd0b66afadbf46a
superznake <108449247+superznake@users.noreply.github.com>
2026-04-21 11:10:55 +0300

    Update upd.sh

--- upd.sh
+++ upd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker rm -f plague_brewing
+git stash
 git pull origin main
 docker build -t plague_brewing:latest .
 docker run --name plague_brewing -p 127.0.0.1:63000:63421 -v $(pwd):/app plague_brewing:latest
